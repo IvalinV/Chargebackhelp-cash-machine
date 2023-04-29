f6987f48786607f9c518a065a6f488a7747278df
Ivalin Venkov <ivalinvenkov@gmail.com>
2023-04-29 09:21:22 +0300

    First commit

---
+++ app/Http/Controllers/CashSourceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCashSourceRequest;
+use App\Http\Requests\UpdateCashSourceRequest;
+use App\Models\CashSource;
+
+class CashSourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCashSourceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CashSource $cashSource)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CashSource $cashSource)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCashSourceRequest $request, CashSource $cashSource)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CashSource $cashSource)
+    {
+        //
+    }
+}
